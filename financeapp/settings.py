ceef620bf20fac2c74c11fc5d4292e2ba3d09a24
Nii Shidaa Adjei <niishidaaadjei781227@gmail.com>
2024-01-15 11:26:37 -0500

    fixing style sheet

--- financeapp/settings.py
+++ financeapp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
